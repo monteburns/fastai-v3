58356bfe520fa63cdd135cb0673f6522540734e5
monteburns <yucehan@gmail.com>
2020-03-23 22:42:49 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -61,11 +61,11 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)
-    if max(prediction[2]) > 0.98:
-        scale='Sapina kadar '
-    if max(prediction[2]) < 0.95:
-        scale='Hafif '
-    return JSONResponse({'Sonuc': scale + str(prediction[0])})
+#    if max(prediction[2]) > 0.98:
+#        scale='Sapina kadar '
+#    if max(prediction[2]) < 0.95:
+#        scale='Hafif '
+    return JSONResponse({'Sonuc': str(prediction[0])})
 
 
 if __name__ == '__main__':
